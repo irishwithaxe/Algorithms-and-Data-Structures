8424b2a992434454e06b2fff6ae17ddc6fc9963f
Nikolay Ilyin <irishwithaxe@gmail.com>
2016-12-12 22:03:12 +0300

    Модифицирована замерялка времени
    Для наглядности и красоты внешней

--- AlgorithmsDataStructures/TimeMeasurements/Program.cs
+++ AlgorithmsDataStructures/TimeMeasurements/Program.cs
@@ -9,40 +9,55 @@ using System.Threading.Tasks;
 using Toster;
 
 namespace TimeMeasurements {
-   class Program {
-      static long length = 15000L;
+   public class Program {
+      static long length = 50L;
       static CancellationTokenSource _cts = new CancellationTokenSource();
 
-      static void Main(string[] args) {
-         var task = Task.Factory.StartNew(() => {
-            long iteratin = 1L;
+      static long iteration = 1L;
+      static Dictionary<string, FinalSortMeasure> measure = new Dictionary<string, FinalSortMeasure>();
 
-            "arrey length {0}.   {1} iteration. press any key to exit. \n".wl(length, iteratin++);
-            var times = MakeMeasure(_cts.Token);
+      public static void Main(string[] args) {
+         var times = MakeMeasure(_cts.Token);
+         iteration++;
+         foreach (var time in times) measure.Add(time.SortName, new FinalSortMeasure(time));
+         Print(measure);
 
-            foreach (var t in times)
-               "{0}: {1} ms".wl(t.SortName, TimeSpan.FromTicks(t.Ticks).TotalMilliseconds);
+         var tasks = new Task[3];
+         for (int i = 0; i < tasks.Length; i++)
+            tasks[i] = Task.Factory.StartNew(RunMeasures, _cts.Token);
 
-            while (!_cts.IsCancellationRequested) {
-               var times1 = MakeMeasure(_cts.Token);
-               Console.Clear();
-
-               "arrey length {0}.   {1} iteration. press any key to exit. \n".wl(length, iteratin++);
-
-               for (int i = 0; i < times.Length; i++)
-                  times[i].Ticks = (times[i].Ticks + times1[i].Ticks) / 2;
-
-               foreach (var t in times)
-                  "{0}: {1} ms".wl(t.SortName, TimeSpan.FromTicks(t.Ticks).TotalMilliseconds);
-            }
-         }, _cts.Token);
+         var timer = new Timer((obj) => { lock (measure) Print(measure); }, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
 
          Console.ReadKey(true);
+
+         timer.Dispose();
          _cts.Cancel();
-         try { task.Wait(); }
-         catch { };
+
+         foreach (var task in tasks)
+            try { task.Wait(); } catch { };
 
          "cancelled".wl();
+      }
+
+      private static void RunMeasures() {
+         while (!_cts.IsCancellationRequested) {
+            var times = MakeMeasure(_cts.Token);
+            iteration++;
+            lock (measure) {
+               foreach (var time in times) measure[time.SortName].Ticks = time.Ticks;
+               //Print(measure);
+            }
+         }
+      }
+
+      private static void Print(Dictionary<string, FinalSortMeasure> measure) {
+         Console.Clear();
+         "arrey length {0}.   {1} iteration. press any key to exit. \n".wl(length, iteration);
+         foreach (var t in measure.Values.OrderBy(x => x.Ticks))
+            "{0}: {1:F4} ms; min {2:F4} ms; max: {3:F4} ms".wl(t.SortName,
+               TimeSpan.FromTicks(t.Ticks).TotalMilliseconds,
+               TimeSpan.FromTicks(t.TicksMin).TotalMilliseconds,
+               TimeSpan.FromTicks(t.TicksMax).TotalMilliseconds);
       }
 
       private static SortMeasure[] MakeMeasure(CancellationToken ct) {
@@ -53,7 +68,8 @@ namespace TimeMeasurements {
          var watcher = new Stopwatch();
 
          Func<Action, TimeSpan> _makeSort = (sort) => {
-            ct.ThrowIfCancellationRequested();
+            if (ct.IsCancellationRequested)
+               return TimeSpan.FromSeconds(0);
 
             etalon.CopyTo(array, 0L);
 
@@ -74,6 +90,8 @@ namespace TimeMeasurements {
          times.Add(new SortMeasure("Insertion.Sort          ", _makeSort(() => Insertion.Sort(array, isSorted)).Ticks));
          times.Add(new SortMeasure("Insertion.ShellSort     ", _makeSort(() => Insertion.ShellSort(array, isSorted)).Ticks));
          times.Add(new SortMeasure("Selection.Sort          ", _makeSort(() => Selection.Sort(array, isSorted)).Ticks));
+         times.Add(new SortMeasure("Heapsort.Sort           ", _makeSort(() => Heapsort.Sort(array)).Ticks));
+         times.Add(new SortMeasure("MergeSort.Sort          ", _makeSort(() => MergeSort.SortDescending(array)).Ticks));
 
          return times.ToArray();
       }
@@ -84,6 +102,31 @@ namespace TimeMeasurements {
             Ticks = ticks;
          }
          public long Ticks;
+         public string SortName;
+      }
+
+      class FinalSortMeasure {
+         public FinalSortMeasure(SortMeasure measure) {
+            SortName = measure.SortName;
+            TicksMax = measure.Ticks;
+            TicksMin = measure.Ticks;
+            Ticks = measure.Ticks;
+            n = 1;
+         }
+
+         public double TicksSum;
+         public long Ticks {
+            get { return Convert.ToInt64(TicksSum / n); }
+            set {
+               TicksMax = TicksMax < value ? value : TicksMax;
+               TicksMin = TicksMin > value ? value : TicksMin;
+               TicksSum += value;
+               n++;
+            }
+         }
+         public long TicksMin;
+         public long TicksMax;
+         public long n = 0;
          public string SortName;
       }
    }
